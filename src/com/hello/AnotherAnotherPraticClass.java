1faccc5435d613f7cf496c654a16504534a22e1a
ghulamalifarzam <alifarzam11@gmail.com>
2023-06-05 14:24:45 -0400

    another another class added

---
+++ src/com/hello/AnotherAnotherPraticClass.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class AnotherAnotherPraticClass {
+
+	public static void main(String[] args) {
+		System.out.println("Another Another Practice Class added");
+
+	}
+
+}
